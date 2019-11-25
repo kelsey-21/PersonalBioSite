1e71009d7d753c76a9f00db13da7e41fd84198e4
Kelsey Creel <kelseycreel@gmail.com>
2019-11-24 18:27:58 -0600

    WIP

--- src/javascripts/components/projects/singleProject/singleProject.js
+++ src/javascripts/components/projects/singleProject/singleProject.js
@@ -1,23 +1,13 @@
 const singleProjectCard = (project) => {
   const domString = `
-  <div class="mdc-card">
-  <div class="mdc-card__primary-action">
-    <div class="mdc-card__media mdc-card__media--square">
-      <div class="mdc-card__media-content">${project.title}</div>
-    </div>
-    <!-- ... additional primary action content ... -->
+  <div class="card col-sm-5 single-project">
+  <img src="${project.img1}" class="card-img-top" alt="...">
+  <div class="card-body hide">
+    <h5 class="card-title">${project.title}</h5>
+    <a href="${project.url}" class="btn btn-primary">See More</a>
+    <a href="${project.githubUrl}" class="btn btn-primary">See Github</a>
   </div>
-  <div class="mdc-card__actions">
-    <div class="mdc-card__action-buttons">
-      <a href="${project.url}" class="mdc-button mdc-card__action mdc-card__action--button">
-        <span class="mdc-button__label">See More</span>
-      </a>
-      <a href="${project.githubUrl}" class="mdc-button mdc-card__action mdc-card__action--button">
-        <span class="mdc-button__label">see Github</span>
-      </a>
-    </div>
   </div>
-</div>
   `;
   return domString;
 };
