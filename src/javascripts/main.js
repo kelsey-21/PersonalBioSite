339bc62ee0ce449a0ae13f18d204a9af10f284b2
Kelsey Creel <kelseycreel@gmail.com>
2019-12-07 16:33:31 -0600

    WIP

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,9 +1,11 @@
+// import $ from 'jquery';
 import projects from './components/projects/projects';
 import click from './components/click/click';
 import tech from './components/tech/tech';
 import bio from './components/bio/bio';
 
 import '../styles/main.scss';
+
 
 const init = () => {
   projects.printProjectCards();
