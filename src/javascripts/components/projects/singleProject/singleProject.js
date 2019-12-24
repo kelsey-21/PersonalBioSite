99769c229f42455f4f1026628f34e448b5468eb2
Kelsey Creel <kelseycreel@gmail.com>
2019-12-23 18:55:05 -0600

    removed video

--- src/javascripts/components/projects/singleProject/singleProject.js
+++ src/javascripts/components/projects/singleProject/singleProject.js
@@ -2,11 +2,10 @@ const singleProjectCard = (project) => {
   const domString = `
   <div class="card single-project border-0">
   <div class="row no-gutters">
-    <div class="col-md-7">
-      <iframe src="${project.video}" frameborder="0" allowfullscreen scrolling="no"></iframe>
-      </iframe>
+    <div class="col-md-6">
+      <img src="${project.video}" alt="..." />
     </div>
-    <div class="col-md-5">
+    <div class="col-md-6">
       <div class="card-body">
         <h5 class="card-title">${project.title}</h5>
         <p class="card-text">${project.description}</p>
