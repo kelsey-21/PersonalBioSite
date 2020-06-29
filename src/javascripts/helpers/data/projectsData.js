e704415c13e338e148f6f0130b5c96ca8b8f7505
Kelsey Creel <kelseycreel@gmail.com>
2020-06-28 21:47:33 -0500

    WIP

--- src/javascripts/helpers/data/projectsData.js
+++ src/javascripts/helpers/data/projectsData.js
@@ -20,4 +20,21 @@ const getProjects = () => new Promise((resolve, reject) => {
     .catch((error) => reject(error));
 });
 
-export default { getProjects };
+const getCapstones = () => new Promise((resolve, reject) => {
+  axios.get(`${baseUrl}/capstones.json`)
+    .then((response) => {
+      const allProjects = response.data;
+      const projects = [];
+      if (allProjects != null) {
+        Object.keys(allProjects).forEach((projectId) => {
+          const newProject = allProjects[projectId];
+          newProject.id = projectId;
+          projects.push(newProject);
+        });
+      }
+      resolve(projects);
+    })
+    .catch((error) => reject(error));
+});
+
+export default { getCapstones, getProjects };
